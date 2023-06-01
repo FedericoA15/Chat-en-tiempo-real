5e5672f3024d1417e5b2e7a4411f40f792c03f75
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-31 23:54:10 -0300

    add suer services

---
+++ ChatTiempoReal/api/services/user_services.py
@@ -0,0 +1,12 @@
+from models.users import UserCreate
+from database.db import get_database
+from bson import ObjectId
+
+db = get_database()
+
+
+def create_user(user: UserCreate):
+    user_data = user.dict()
+    user_data["_id"] = str(ObjectId())
+    db["users"].insert_one(user_data)
+    return user_data
