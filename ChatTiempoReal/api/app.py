32b0f8866b096d2d0ff5950aacc5648554899bc9
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-05-31 23:54:20 -0300

    fix app

--- ChatTiempoReal/api/app.py
+++ ChatTiempoReal/api/app.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
 from database.db import get_database
+from routers.users import router as users_router
 
 app = FastAPI()
 
 db = get_database()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Registrar los enrutadores
+app.include_router(users_router)
 
 
 if __name__ == "__main__":
