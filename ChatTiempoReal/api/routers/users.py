c3abb19d14c299d44df4c1716b9fb2796e073b12
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-06-01 22:55:45 -0300

    add user

--- ChatTiempoReal/api/routers/users.py
+++ ChatTiempoReal/api/routers/users.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from models.users import UserCreate
-from services.user_services import create_user
-from validations.user_validation import validate_user_create
+from services.user_services import create_user, email_exists
 
 router = APIRouter(prefix="/users", tags=["products"])
 
 
 @router.post("/")
 def create_user_route(user: UserCreate):
-    print("Antes de ejecutar el validador")
-    validate_user_create(user)
+    email = user.email
+
+    if email_exists(email):
+        raise HTTPException(status_code=400, detail="Email already exists")
+
     created_user = create_user(user)
     return created_user
 
