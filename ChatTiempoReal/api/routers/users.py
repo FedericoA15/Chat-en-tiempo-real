21c62a5e14af8110303434becdd6bbedf80ca91b
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-06-02 19:03:07 -0300

    ADD router get_user_by_id_route

--- ChatTiempoReal/api/routers/users.py
+++ ChatTiempoReal/api/routers/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from models.users import UserCreate
-from services.user_services import create_user, get_all_users
+from services.user_services import create_user, get_all_users, get_user_by_id
 
 router = APIRouter(prefix="/users", tags=["products"])
 
@@ -14,3 +14,12 @@ def create_user_route(user: UserCreate):
 @router.get("/")
 def get_all_users_route():
     return get_all_users()
+
+
+@router.get("/{user_id}")
+def get_user_by_id_route(user_id: str):
+    user = get_user_by_id(user_id)
+    if user:
+        return user
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
