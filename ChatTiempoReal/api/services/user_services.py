f6a069d564ad8d3773239ce1885853007f0a62a4
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-06-02 17:50:47 -0300

    fixx final en user

--- ChatTiempoReal/api/services/user_services.py
+++ ChatTiempoReal/api/services/user_services.py
@@ -2,12 +2,21 @@ from models.users import UserCreate
 from database.db import get_database
 from bson import ObjectId
 from fastapi import HTTPException
+from pymongo.errors import DuplicateKeyError
 
 db = get_database()
 
 
 def create_user(user: UserCreate):
     user_data = user.dict()
+
+    if db["users"].find_one({"email": user_data["email"]}):
+        raise HTTPException(status_code=400, detail="Email already registered")
+
     user_data["_id"] = str(ObjectId())
-    db["users"].insert_one(user_data)
+    try:
+        db["users"].insert_one(user_data)
+    except DuplicateKeyError:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
     return user_data
