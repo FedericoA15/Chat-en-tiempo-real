a3687fa87f4178e536ca7ca720a3ecae168acfc8
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-06-07 18:59:28 -0300

    fix de backend api

--- ChatTiempoReal/api/app.py
+++ ChatTiempoReal/api/app.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.websockets import WebSocketDisconnect
 import uvicorn
 from database.db import get_database
-from routers.users import router as users_router
 import json
 
 app = FastAPI()
 
+# Configurar los orígenes permitidos para CORS
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+    # Agrega aquí los orígenes permitidos para tu frontend
+]
+
+# Agregar el middleware de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
 db = get_database()
 websocket_connections = []
-
-# Registrar los enrutadores
-app.include_router(users_router)
 
 
 # Función para guardar el mensaje en la base de datos
@@ -33,21 +46,15 @@ def save_message(sender_id: str, recipient_id: str, message: str):
         # Actualizar el documento existente con el nuevo mensaje
         collection.update_one(
             {"_id": conversation["_id"]},
-            {"$push": {"messages": json.loads(message)["message"]}},
+            {"$push": {"messages": message}},
         )
     else:
         # Crear un nuevo documento de conversación y agregar el mensaje
         conversation_data = {
             "participants": [sender_id, recipient_id],
-            "messages": [json.loads(message)["message"]],
+            "messages": [message],
         }
         collection.insert_one(conversation_data)
-
-
-# Ruta principal
-@app.get("/")
-def home():
-    return {"message": "Bienvenido a la aplicación de chat"}
 
 
 def get_chat_messages(sender_id: str, recipient_id: str):
@@ -105,33 +112,13 @@ async def chat_websocket(websocket: WebSocket, user_id: str, recipient_id: str):
             # Guardar el mensaje en la base de datos
             save_message(user_id, recipient_id, message)
 
-            # Enviar el mensaje al usuario actual
-            await websocket.send_json(
-                {"userId": user_id, "recipient_id": recipient_id, "message": message}
-            )
-
-            # Buscar la conexión del destinatario
-            recipient_connection = next(
-                (
-                    connection
-                    for connection in websocket_connections
-                    if connection["user_id"] == recipient_id
-                ),
-                None,
-            )
-
-            if recipient_connection:
-                # Enviar el mensaje al destinatario
-                await recipient_connection["websocket"].send_json(
-                    {
-                        "userId": user_id,
-                        "recipient_id": recipient_id,
-                        "message": message,
-                    }
-                )
+            # Enviar el mensaje a todos los clientes conectados en el chat
+            for connection in websocket_connections:
+                if connection["user_id"] == recipient_id:
+                    await connection["websocket"].send_text(message)
 
     except WebSocketDisconnect:
-        # Remover la conexión WebSocket al desconectarse
+        # Eliminar la conexión WebSocket cuando el cliente se desconecta
         websocket_connections = [
             connection
             for connection in websocket_connections
@@ -139,30 +126,5 @@ async def chat_websocket(websocket: WebSocket, user_id: str, recipient_id: str):
         ]
 
 
-@app.get("/chat/{user_id}/{recipient_id}")
-@app.get("/chat/{user_id}/{recipient_id}")
-def get_chat_conversation(user_id: str, recipient_id: str):
-    collection = db["chat_conversations"]
-
-    conversation = collection.find_one(
-        {
-            "$or": [
-                {"participants": [user_id, recipient_id]},
-                {"participants": [recipient_id, user_id]},
-            ]
-        },
-        {"messages": 1, "_id": 0},  # Proyectar solo el campo "messages" y excluir "_id"
-    )
-
-    if conversation:
-        return conversation["messages"]
-    else:
-        return {"message": "Conversación no encontrada"}
-
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
-if __name__ == "__main__":
-    print("Iniciando el servidor...")
     uvicorn.run(app, host="0.0.0.0", port=8000)
