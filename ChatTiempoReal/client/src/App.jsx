6ba1dee48bc63e3f3f7b9a72be0f755abb482bba
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-06-07 02:08:46 -0300

    fix para fornt chat

--- ChatTiempoReal/client/src/App.jsx
+++ ChatTiempoReal/client/src/App.jsx
@@ -1,105 +1,70 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { w3cwebsocket as WebSocket } from "websocket";
 
 const App = () => {
-  const [messages, setMessages] = useState([]);
-  const [inputMessage, setInputMessage] = useState("");
-  const [loading, setLoading] = useState(true);
   const userId = "6478c00321f12c3a7d8b7d2b";
   const recipientId = "6478064a6715d5ab6d35643e";
-  const websocketRef = useRef(null);
+  const socket = new WebSocket(`ws://127.0.0.1:8000/chat/${userId}/${recipientId}`);
+
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
-    let websocket = websocketRef.current;
-
-    if (!websocket) {
-      websocket = new WebSocket("ws://127.0.0.1:8000/chat/" + userId + "/" + recipientId);
-      websocketRef.current = websocket;
-    }
-
-    const handleOpen = () => {
-      console.log("WebSocket connection established.");
+    // Establecer los listeners de eventos
+    socket.onopen = () => {
+      console.log("Conexión WebSocket establecida");
     };
 
-    const handleMessage = (message) => {
-      const messageData = JSON.parse(message.data);
-      console.log(messageData.message);
-
-      const newMessage = {
-        sender_id: messageData.userId,
-        message: messageData.message,
-      };
-
-      setMessages((prevMessages) => [...prevMessages, newMessage]);
+    socket.onmessage = (message) => {
+      const data = JSON.parse(message.data);
+      setMessages((prevMessages) => [...prevMessages, data]);
     };
 
-    const handleClose = () => {
-      console.log("WebSocket connection closed.");
+    socket.onclose = () => {
+      console.log("Conexión WebSocket cerrada");
     };
 
-    websocket.addEventListener("open", handleOpen);
-    websocket.addEventListener("message", handleMessage);
-    websocket.addEventListener("close", handleClose);
+    // Obtener los mensajes al montar el componente
+    getMessages();
 
     return () => {
-      websocket.removeEventListener("open", handleOpen);
-      websocket.removeEventListener("message", handleMessage);
-      websocket.removeEventListener("close", handleClose);
+      // Cerrar la conexión WebSocket al desmontar el componente
+      // socket.close();
     };
-  }, [userId, recipientId]);
+  }, []);
 
-  useEffect(() => {
-    // Obtener los mensajes anteriores desde el backend al cargar la página
-    const fetchChatMessages = async () => {
-      try {
-        const response = await fetch(
-          "http://localhost:8000/chat_messages?sender_id=" + userId + "&recipient_id=" + recipientId
-        );
-        const data = await response.json();
-        setMessages(data);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching chat messages:", error);
-      }
-    };
+  const getMessages = () => {
+    const { data } = axios
+      .get(`http://127.0.0.1:8000/chat/${userId}/${recipientId}`)
+      .catch((error) => {
+        console.error("Error al obtener los mensajes:", error);
+      });
+  };
 
-    fetchChatMessages();
-  }, [userId, recipientId]);
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
   const sendMessage = () => {
-    const message = {
-      userId: userId,
-      recipient_id: recipientId,
-      message: inputMessage,
-    };
-    websocketRef.current.send(JSON.stringify(message));
-
-    setInputMessage("");
+    if (inputValue.trim() !== "") {
+      socket.send(inputValue);
+      setInputValue("");
+    }
   };
 
   return (
     <div>
-      {loading ? (
-        <div>Cargando mensajes...</div>
-      ) : (
-        <div>
-          {messages.map((message, index) => (
-            <div
-              key={index}
-              style={{
-                textAlign: message.sender_id === userId ? "right" : "left",
-                margin: "10px 0",
-              }}
-            >
-              {message.message}
-            </div>
-          ))}
-        </div>
-      )}
+      <h1>Chat en tiempo real</h1>
       <div>
-        <input type='text' value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} />
-        <button onClick={sendMessage}>Send</button>
+        {messages.map((message, index) => (
+          <div key={index}>
+            <p>{message.message}</p>
+          </div>
+        ))}
       </div>
+      <input type='text' value={inputValue} onChange={handleInputChange} />
+      <button onClick={sendMessage}>Enviar</button>
     </div>
   );
 };
