55bfb3a1bbcea0a30206e041faf2568177f39f86
federicoalmeida15@gmail.com <federicoalmeida15@gmail.com>
2023-06-08 00:52:51 -0300

    guardar en la database los mensajes

--- ChatTiempoReal/api/app.py
+++ ChatTiempoReal/api/app.py
@@ -1,7 +1,7 @@
-# main.py
 from fastapi import FastAPI
 from fastapi import WebSocket
 from typing import Dict
+from database.db import get_database
 
 app = FastAPI()
 
@@ -17,9 +17,29 @@ async def chat_endpoint(websocket: WebSocket, sender_id: str, recipient_id: str)
     # Asignar el WebSocket a la conexión del remitente
     connections[sender_id] = websocket
 
+    db = get_database()
+
+    # Buscar el documento de la conversación
+    conversation = db.conversations.find_one(
+        {"participants": {"$all": [sender_id, recipient_id]}}
+    )
+
+    if conversation is None:
+        # Crear un nuevo documento para la conversación si no existe
+        conversation = {"participants": [sender_id, recipient_id], "messages": []}
+        db.conversations.insert_one(conversation)
+
     while True:
         message = await websocket.receive_text()
         # Lógica para procesar y guardar los mensajes en la base de datos
+        message_data = {
+            "sender_id": sender_id,
+            "recipient_id": recipient_id,
+            "message": message,
+        }
+        db.conversations.update_one(
+            {"_id": conversation["_id"]}, {"$push": {"messages": message_data}}
+        )
 
         # Enviar el mensaje al destinatario si está conectado
         recipient_connection = connections.get(recipient_id)
